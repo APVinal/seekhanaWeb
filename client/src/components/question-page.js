fc9c45631a190f8b982fc82ffc8ab99590d993c4
Paton Vinal <paton.vinal@gmail.com>
2017-08-09 14:18:49 -0400

    504 error when logging in

--- client/src/components/question-page.js
+++ client/src/components/question-page.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import LinkedList from '../linkedList';
 
 class QuestionPage extends Component {
 
   componentWillMount(){
-    //dispatch(makeLL())
+    let newLesson = new LinkedList();
+  
+    let i = 0;
+
+    this.findLesson(this.props.lesson, this.props.lessonId).questions.forEach(question => {
+      newLesson.insert(i++, question);
+    });
+
+    this.lesson = newLesson;
+  }
+
+  findLesson(arr, id) {
+    for(let i = 0; i < arr.length; i++) {
+      if (arr[i]._id === id) {
+        return arr[i];
+      }
+    }
   }
 
   render() {
+    console.log(this.lesson);
       return (
         <div>
           <p>this is where the quiz goes</p>
